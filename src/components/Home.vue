624da7691dab18a93368f510dbc1d4f7e3c9dbd6
ZPM <1098466927@qq.com>
2020-04-27 17:54:50 +0800

    提交代码

---
+++ src/components/Home.vue
@@ -0,0 +1,185 @@
+<template>
+  <el-container class="home-container">
+    <!--头部-->
+    <el-header>
+      <div>
+        <span style="margin-left:20px;">
+          <img
+            src="../assets/ilogo.png"
+            width="250px;"
+            style="margin-left:-10px;"
+            height="100%;"
+            alt
+            srcset
+          />
+        </span>
+      </div>
+
+      <el-dropdown>
+        <div class="block">
+          <el-avatar :size="50" :src="this.userInfo.avatar" style="cursor: pointer;"></el-avatar>
+        </div>
+
+        <el-dropdown-menu slot="dropdown" trigger="click">
+          <el-dropdown-item>
+            <span type="danger" @click="logout">退出登入</span>
+          </el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </el-header>
+    <!--主体-->
+    <el-container style="height: 500px;">
+      <!--菜单-->
+      <el-aside :width="isOpen==true?'64px':'200px'">
+        <div class="toggle-btn" @click="toggleMenu">|||</div>
+        <el-menu
+          class="el-menu-vertical-demo"
+          :collapse="isOpen"
+          :router="true"
+          :default-active="activePath"
+          background-color="#303030"
+          :collapse-transition="false"
+          text-color="rgba(255,255,255,0.7)"
+          unique-opened
+        >
+          <Menu  :menuList="this.menuList"></Menu>
+        </el-menu>
+      </el-aside>
+   
+      <!--右边主体-->
+      <el-main v-loading="loading">
+     
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+import Menu from "./Menu.vue"; //引进菜单模板
+
+export default {
+  data() {
+    return {
+      loading: true,
+      activePath: "", //激活的路径
+      isOpen: false,
+      menuList: {},
+      userInfo: {},
+     
+    };
+  },
+  components: {
+    Menu
+  },
+  methods: {
+    /**
+     *
+     * 退出登入
+     */
+    async logout() {
+      var res = await this.$confirm("此操作将退出系统, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      }).catch(() => {
+        this.$message({
+          type: "info",
+          message: "已取消退出登入"
+        });
+      });
+      if (res == "confirm") {
+        window.localStorage.clear();
+        this.$router.push("/login");
+      }
+    },
+    /**
+      加载菜单数据
+     */
+    async getMenuList() {
+      const { data: res } = await this.$http.get("user/findMenu");
+      if (res.code !== 200)
+        return this.$message.error("获取菜单失败:" + res.msg);
+       this.menuList = res.data;
+    },
+    /**
+      获取用户信息
+     */
+    async getUserInfo() {
+      const { data: res } = await this.$http.get("user/info");
+      if (res.code !== 200) {
+        return this.$message.error("获取用户信息失败:" + res.msg);
+      } else {
+        var urls = [];
+        res.data.menus.forEach((item, index, array) => {
+          //执行代码
+          if (item != null && item != "") {
+            urls.push(item.url);
+          }
+        });
+        var m = urls.filter(s => {
+          return s && s.trim();
+        });
+        this.userInfo = res.data;
+        //保存用户
+        this.$store.commit("setUserInfo", res.data);
+      }
+    },
+    /**
+     * 菜单伸缩
+     */
+    toggleMenu() {
+      this.isOpen = !this.isOpen;
+    },
+  
+  },
+  mounted() {
+    this.getUserInfo();
+  },
+  created() {
+    this.getMenuList();
+    this.activePath = window.sessionStorage.getItem("activePath");
+    // if(window.sessionStorage.getItem("activePath")==null){
+    //   this.activePath='/welcome';
+    // }
+    setTimeout(() => {
+      this.loading = false;
+    }, 500);
+  }
+};
+</script>
+
+<style>
+/* 为对应的路由跳转时设置动画效果 */
+
+.el-header {
+  background-color: #303030;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+  font-size: 19px;
+
+  padding-left: 0px;
+}
+.el-aside {
+  background-color: #303030;
+}
+.el-main {
+  background-color: #eaedf1;
+}
+.home-container {
+  width: 100%;
+  height: 100% !important;
+}
+.toggle-btn {
+  background-color: rgba(87, 0, 185, 0.884);
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+}
+
+</style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,179 @@
+<template>
+  <div id="welcome">
+    <el-breadcrumb separator="/" style="padding-left:10px;padding-bottom:10px;font-size:12px;">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>欢迎</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-row :gutter="20">
+      <el-col :span="12">
+        <div class="grid-content bg-purple-light">
+          <el-card class="box-card">
+            <div slot="header" class="clearfix">
+              <span>用户信息</span>
+              <el-button style="float: right;" size="mini" type="danger">用户中心</el-button>
+            </div>
+            <el-tooltip class="item" effect="dark" content="Top Left 提示文字" placement="top-start">
+              <el-avatar
+                shape="square"
+                :size="60"
+                :src="userInfo.avatar"
+                style="float:left;"
+                :key="1"
+              ></el-avatar>
+            </el-tooltip>
+            <div class="right" style="float:right;width:520px;">
+              <el-table :data="tableInfo" border height="120">
+                <el-table-column prop="userName" label="用户账号"></el-table-column>
+                <el-table-column prop="nickName" label="用户昵称"></el-table-column>
+                <el-table-column prop="department" label="所属部门"></el-table-column>
+                <el-table-column fixed="right" prop="roles" label="用户角色" width="150"></el-table-column>
+              </el-table>
+            </div>
+          </el-card>
+
+          <el-row style="margin-top:10px;" :gutter="10">
+            <el-card style="height:125px;">
+              <el-col :span="6">
+                <div class="grid-content bg-purple">
+                  <router-link to="/products">
+                    <img
+                      src="../assets/11.png"
+                      alt
+                      width="60.8px;"
+                      style="margin:0px auto; cursor: pointer;margin-left:20px;"
+                    />
+                  </router-link>
+                  <div style="font-size:12px;margin-top:5px;margin-left:25px;">物资资料</div>
+                </div>
+              </el-col>
+              <el-col :span="6">
+                <div class="grid-content bg-purple-light">
+                  <router-link to="/stocks">
+                    <img
+                      src="../assets/22.png"
+                      alt
+                      width="60.8px;"
+                      style="cursor: pointer;margin-left:20px;"
+                    />
+                  </router-link>
+                  <div style="font-size:12px;margin-top:5px;margin-left:25px;">物资库存</div>
+                </div>
+              </el-col>
+
+              <el-col :span="6">
+                <div class="grid-content bg-purple-light">
+                  <router-link to="/inStocks">
+                    <img
+                      src="../assets/44.png"
+                      alt
+                      width="60.8px;"
+                      style="cursor: pointer;margin-left:20px;"
+                    />
+                  </router-link>
+
+                  <div style="font-size:12px;margin-top:5px;margin-left:25px;">物资入库</div>
+                </div>
+              </el-col>
+              <el-col :span="6">
+                <div class="grid-content bg-purple"></div>
+
+                <img
+                  src="../assets/33.png"
+                  alt
+                  width="60.8px;"
+                  style="cursor: pointer;margin-left:20px;"
+                />
+                <div style="font-size:12px;margin-top:5px;margin-left:25px;">物资发放</div>
+              </el-col>
+            </el-card>
+          </el-row>
+          <el-card class="box-card" style="margin-top:20px;">
+            <div slot="header" class="clearfix">
+              <span>轮播图</span>
+              <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+            </div>
+            <div class="text item">
+              <div class="block">
+                <el-carousel height="150px">
+                  <el-carousel-item v-for="item in 4" :key="item">
+                    <h3 class="small"></h3>
+                  </el-carousel-item>
+                </el-carousel>
+              </div>
+              <div class="block"></div>
+            </div>
+          </el-card>
+        </div>
+      </el-col>
+
+      <el-col :span="12">
+        <div class="grid-content bg-purple">
+          <el-card class="box-card">
+            <div slot="header" class="clearfix">
+              <span>日历</span>
+              <el-button style="float: right; padding: 3px 0" type="text">添加日程</el-button>
+            </div>
+            <div class="text item">
+              <el-calendar>
+                <!-- 这里使用的是 2.5 slot 语法，对于新项目请使用 2.6 slot 语法-->
+                <template slot="dateCell" slot-scope="{date, data}">
+                  <p
+                    :class="data.isSelected ? 'is-selected' : ''"
+                  >{{ data.day.split('-').slice(1).join('-') }} {{ data.isSelected ? '✔️' : ''}}</p>
+                </template>
+              </el-calendar>
+            </div>
+          </el-card>
+        </div>
+      </el-col>
+    </el-row>
+
+    <!-- <el-card class="box-card">
+           <el-calendar v-model="value"></el-calendar>
+    </el-card>-->
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      value: new Date(),
+      userInfo: {},
+      tableInfo: []
+    };
+  },
+  created() {
+    this.userInfo = this.$store.state.userInfo;
+    var roles = "";
+    if (this.userInfo.roles.length > 0) {
+      this.userInfo.roles.forEach(item => {
+        roles += item.roleName + ",";
+      });
+    }
+    this.tableInfo.push({
+      userName: this.userInfo.userName,
+      nickName: this.userInfo.nickName,
+      department: this.userInfo.department,
+      roles: roles.substring(0, roles.length - 1)
+    });
+  }
+};
+</script>
+
+<style scoped>
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 200px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+</style>
